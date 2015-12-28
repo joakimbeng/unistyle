99f0a164f5d6023fd8e25d28522bb5a50139d178
Joakim Carlstein <joakim.carlstein@gmail.com>
2015-12-28 18:14:36 +0100

    Verifying that browser prefixed properties are not dasherized correctly (#11)

--- test/fixtures/forms/button.js
+++ test/fixtures/forms/button.js
@@ -3,6 +3,8 @@ import {palm} from '../vars/mediaqueries';
 
 export default {
   'button, .button': {
+    WebkitTransform: 'translate(1px)',
+    transform: 'translate(1px)',
     padding: 15,
     border: `2px solid ${dark}`,
     [palm]: {
