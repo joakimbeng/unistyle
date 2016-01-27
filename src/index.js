b4177378570172c5615be16b11d34272809532f4
Joakim Carlstein <joakim.carlstein@gmail.com>
2016-01-27 09:18:03 +0100

    Handle Babel compiled modules (fixes #12)

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ var flat = require('unistyle-flat');
 var pixelify = require('pixelify');
 var kebabCase = require('kebab-case');
 
+function getExport(obj) {
+  if (obj.__esModule) {
+    return obj.default || obj;
+  }
+  return obj;
+}
+
 module.exports = function compile(obj) {
   return new Promise(function (resolve) {
     if (typeof obj === 'string') {
       obj = require(path.resolve(obj));
     }
+    obj = getExport(obj);
     resolve(toCss(flat(obj), {
       indent: '\t',
       property: kebabCase,
